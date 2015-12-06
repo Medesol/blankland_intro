83a17844906b4e10d2490f83d94d5a9560d8be69
ledenel <ledenelintelli@gmail.com>
2015-12-06 15:38:34 +0900

    create a protocol plugin for remote calling
    now
    character_intro.html?element
    will call the specific element pages.

--- js/carouselprotocol.js
+++ js/carouselprotocol.js
@@ -1,10 +1,9 @@
 $(function () {
     var numindex = location.search.lastIndexOf("?") + 1;
     if (numindex > 0 && numindex < location.search.length) {
-        var carid = parseInt(location.search.substring(numindex));
-        setTimeout(function () {
-            $(".remote-carousel").carousel(carid);
-        }, 300);
-
+        var carid = location.search.substring(numindex);
+        $(".protocol-" + carid).addClass("active");
+    } else {
+        $(".default-protocol").addClass("active");
     }
 });
