a1ce96040ce00c135fc226214619d3b2223a5bd1
ledenel <ledenelintelli@gmail.com>
2015-12-10 22:56:34 +0900

    Add game UI and css style

---
+++ js/cells.js
@@ -0,0 +1,15 @@
+function Cell() {
+    this.pr
+}
+
+function Fog(element) {
+    
+}
+
+function NumberSign(number,position) {
+    
+    this.num = number;
+    this.changeNumber = function (x) {
+        this.num = x;
+    }
+}
